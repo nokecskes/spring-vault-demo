45d3c399428d1c973318943448506ad87d3413f9
nokecskes <nokecskes@gmail.com>
2021-02-22 19:00:19 +0100

    Remove unnecessary property, add description in comments

--- src/main/java/com/springvault/demo/configuration/VaultConfiguration.java
+++ src/main/java/com/springvault/demo/configuration/VaultConfiguration.java
@@ -4,7 +4,34 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.vault.config.EnvironmentVaultConfiguration;
 
+/*
+ * EnvironmentVaultConfiguration allows configuration by using properties from existing property sources and Spring's Environment.
+ * Here: application.properties
+ * 
+ * Can configure:
+ * - Vault URI
+ * - SSL configuration
+ * - authentication method (default is TOKEN) and its configuration
+ */
 @Configuration
 @Import(value = EnvironmentVaultConfiguration.class)
 public class VaultConfiguration {
 }
+
+/*
+ * We can configure Vault with extending AbstractVaultConfiguration:
+ * 
+ * It's a supporting configuration class that provides bean definitions Should
+ * implement: vaultEndpoint() and clientAuthentication()
+ *
+ * Example:
+ * 
+ * @Configuration public class VaultConfig extends AbstractVaultConfiguration {
+ *
+ * @Override public ClientAuthentication clientAuthentication() { return new
+ * TokenAuthentication("00000000-0000-0000-0000-000000000000"); }
+ *
+ * @Override public VaultEndpoint vaultEndpoint() { return
+ * VaultEndpoint.create("host", 8020); } }
+ * 
+ */
--- src/main/java/com/springvault/demo/credentials/CredentialService.java
+++ src/main/java/com/springvault/demo/credentials/CredentialService.java
@@ -9,15 +9,52 @@ import org.springframework.vault.support.VaultResponseSupport;
 @Service
 public class CredentialService {
 
+	/*
+	 * VaultTemplate configuration is provided in application.properties.
+	 * 
+	 * We can initialize our VaulTemplate also as:
+	 * 
+	 * VaultTemplate vaultTemplate = new VaultTemplate(new VaultEndpoint(), new
+	 * TokenAuthentication("00000000-0000-0000-0000-000000000000"));
+	 * 
+	 * 
+	 * There are multiple authentication methods available, e.g: token, appId, ASW
+	 * EC2, Azure, Kubernetes etc.
+	 * 
+	 * See: EnvironmentVaultConfiguration.class
+	 * 
+	 * 
+	 * Create new VaultEndpoint:
+	 * 
+	 * A) with default endpoint pointing to http://localhost:8200
+	 * 
+	 * VaultEndpoint endpoint = new VaultEndpoint();
+	 * 
+	 * 
+	 * B) specify Vault host and port
+	 * 
+	 * VaultEndpoint endpoint = VaultEndpoint.create("host", port);
+	 * 
+	 * 
+	 * C) create from Vault URL
+	 * 
+	 * VaultEndpoint endpoint = VaultEndpoint.from(new URI("vault uri"));
+	 * 
+	 */
+
 	private static final String VAULT_DEMO_PATH_NAME = "cubbyhole/demo";
 
 	@Autowired
 	private VaultTemplate vaultTemplate;
 
+	// We proved the Secret Engine's path and the key-value pair (secret) we want to
+	// save.
 	public void secureCredentials(Credentials credentials) {
 		vaultTemplate.write(VAULT_DEMO_PATH_NAME, credentials);
 	}
 
+	// We read secrets by providing the Secret Engine's path.
+	// Mapping to POJO works without any additional metadata.
 	public Credentials accessCredentials() {
 		VaultResponseSupport<Credentials> response = vaultTemplate.read(VAULT_DEMO_PATH_NAME, Credentials.class);
 		return response.getData();
